27609d277c1c4d06d84084b110bfc335c0870833
Vic <vicmisanu63@gmail.com>
2025-01-15 17:26:09 +0100

    some informations changed on the teamPage

--- src/components/Pages/team.tsx
+++ src/components/Pages/team.tsx
@@ -11,18 +11,18 @@ const Team: React.FC = () => {
             </div>
             <div className="flex flex-col items-center py-20">
                 <ProfInfo 
-                    name="Prof. Muvumbi Victoire" 
-                    image="/images/vic1.jpg" 
-                    searchField="Machine learning" 
-                    descript='PhD en machine learning ipsum dolor sit amet consectetur adipisicing elit. At libero, eveniet quod similique deserunt, magni laboriosam porro deleniti minus nihil veritatis voluptatem sed odit natus est magnam consectetur. Officiis, optio!
+                    name="Prof. Lendo Arnold" 
+                    image="/images/arnold.jpg" 
+                    searchField="Sécurité informatique" 
+                    descript='PhD en sécurité informatique ipsum dolor sit amet consectetur adipisicing elit. At libero, eveniet quod similique deserunt, magni laboriosam porro deleniti minus nihil veritatis voluptatem sed odit natus est magnam consectetur. Officiis, optio!
                     dolor sit amet consectetur adipisicing elit. At libero, eveniet quod similique deserunt, magni laboriosam porro deleniti minus nihil veritatis voluptatem sed odit natus est magnam consectetur. Officiis, optio!' 
-                    whatsapp="https://api.whatsapp.com/send?phone=243825057845" 
-                    facebook="https://web.facebook.com/profile.php?id=100077261924800"
+                    whatsapp="https://api.whatsapp.com/send?phone=243827457217" 
+                    facebook="https://web.facebook.com/arnold.lendo.94"
                     linkedin='https://www.linkedin.com/in/victoire-muvumbi-203680309/'
                 />
                 <ProfInfo 
                     name="Prof. Muvumbi Victoire" 
-                    image="/images/vic1.jpg" 
+                    image="/images/vic3.jpg" 
                     searchField="Machine learning" 
                     descript='PhD en machine learning ipsum dolor sit amet consectetur adipisicing elit. At libero, eveniet quod similique deserunt, magni laboriosam porro deleniti minus nihil veritatis voluptatem sed odit natus est magnam consectetur. Officiis, optio!
                     dolor sit amet consectetur adipisicing elit. At libero, eveniet quod similique deserunt, magni laboriosam porro deleniti minus nihil veritatis voluptatem sed odit natus est magnam consectetur. Officiis, optio!' 
